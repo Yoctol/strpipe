7fb32fa1db6f60e86a77b4182f461b951e6b927a
Solumilken <s916526000@gmail.com>
2018-08-27 11:18:55 +0800

    fitted flake8

--- strpipe/toolkit/compute_maxlen.pyx
+++ strpipe/toolkit/compute_maxlen.pyx
@@ -1,7 +1,8 @@
 def compute_maxlen(sentences):
     return compute_maxlen_in_c(sentences)
 
-cdef compute_maxlen_in_c(list sentences):
+
+cdef compute_maxlen_in_c(list sentences):  # noqa: E999
     cdef int maxlen = 0
     cdef list sent
     for i in range(len(sentences)):
