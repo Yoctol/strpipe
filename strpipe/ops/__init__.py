e69b595254f0e4021e83dd138618d79463a95f6e
Solumilken <s916526000@gmail.com>
2018-09-06 17:22:02 +0800

    registered chinese char tokenizer op

--- strpipe/ops/__init__.py
+++ strpipe/ops/__init__.py
@@ -2,8 +2,11 @@ from strpipe.ops.factory import _OpFactory
 
 from strpipe.ops.pad import Pad
 from strpipe.ops.token_to_index import TokenToIndexWithUNK
+from strpipe.ops.zhchar_tokenizer import ZhCharTokenizer
+
 
 op_factory = _OpFactory()
 
 op_factory.register('Pad', Pad)
 op_factory.register('TokenToIndexWithUNK', TokenToIndexWithUNK)
+op_factory.register('ZhCharTokenizer', ZhCharTokenizer)
