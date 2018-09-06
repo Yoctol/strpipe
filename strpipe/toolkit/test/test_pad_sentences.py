711c1f1e272e9f878cea3c487d4f2ac581f2145a
absolutelyNoWarranty <absolutelyNoWarranty@users.noreply.github.com>
2018-09-06 20:10:52 +0800

    Extend test_pad_sentences with failing eos/sos test cases

--- strpipe/toolkit/test/test_pad_sentences.py
+++ strpipe/toolkit/test/test_pad_sentences.py
@@ -17,7 +17,7 @@ def sentences():
 def test_pad_sentences_with_default_pad(sentences):
     output = pad_sentences(
         sentences=sentences,
-        maxlen=3
+        maxlen=3,
     )
     assert output[0] == [
         ["隼興", "覺得", "有顆頭"],
@@ -82,9 +82,48 @@ def test_pad_sentences_with_custom_pad(sentences):
         {'sentlen': 1, 'sentence_tail': []},
         {'sentlen': 0, 'sentence_tail': []}]
     ),
+    ('<YO>', None, None, 3, [
+        ["<YO>", "隼興", "覺得"],
+        ["<YO>", "藍莓", "結冰"],
+        ["<YO>", "薩克斯風", "好用"],
+        ["<YO>", "", "<CPH>"],
+        ["<YO>", "<CPH>", "<CPH>"]], [
+        {'sentlen': 5, 'sentence_tail': ["有顆頭", "有點", "猥瑣"]},
+        {'sentlen': 3, 'sentence_tail': ["惹"]},
+        {'sentlen': 2, 'sentence_tail': []},
+        {'sentlen': 1, 'sentence_tail': []},
+        {'sentlen': 0, 'sentence_tail': []}]
+    ),
+    (None, '<BYE>', None, 3, [
+        ["隼興", "覺得", "<BYE>"],
+        ["藍莓", "結冰", "<BYE>"],
+        ["薩克斯風", "好用", "<BYE>"],
+        ["", "<BYE>", "<PAD>"],
+        ["<BYE>", "<PAD>", "<PAD>"]], [
+        {'sentlen': 5, 'sentence_tail': ["有顆頭", "有點", "猥瑣"]},
+        {'sentlen': 3, 'sentence_tail': ["惹"]},
+        {'sentlen': 2, 'sentence_tail': []},
+        {'sentlen': 1, 'sentence_tail': []},
+        {'sentlen': 0, 'sentence_tail': []}]
+    ),
+    ('<A>', '<Z>', None, 3, [
+        ["<A>", "隼興", "<Z>"],
+        ["<A>", "藍莓", "<Z>"],
+        ["<A>", "薩克斯風", "<Z>"],
+        ["<A>", "", "<Z>"],
+        ["<A>", "<Z>", "<PAD>"]], [
+        {'sentlen': 5, 'sentence_tail': ["覺得", "有顆頭", "有點", "猥瑣"]},
+        {'sentlen': 3, 'sentence_tail': ["結冰", "惹"]},
+        {'sentlen': 2, 'sentence_tail': ["好用"]},
+        {'sentlen': 1, 'sentence_tail': []},
+        {'sentlen': 0, 'sentence_tail': []}]
+    ),
 ], ids=[
     'default_pad',
-    'custom_pad'
+    'custom_pad',
+    'sos',
+    'eos',
+    'sos_and_eos',
 ])
 def test_pad_sentences(sentences, sos, eos, pad, maxlen,
     expected_padded_sentences, expected_meta):
